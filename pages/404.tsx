ae92321545b34efbb95c0713c7798f44f87d4eb3
DoWhatYouL0ve <william_ross@mailfence.com>
2022-11-30 11:47:18 +0100

    feat: created pages - 404 / posts / contacts / contact

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Error = () => {
+    return(
+        <>
+            <h1>404</h1>
+            <p>Something went wrong ...</p>
+        </>
+    )
+}
+
+export default Error
---
+++ pages/contacts/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Contacts = () => {
+    return(
+        <>
+            <h1>Contacts:</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda at, aut beatae consequatur cum dicta dolore doloribus eaque eum eveniet explicabo facere facilis fuga fugit hic illo inventore ipsam iusto labore magnam maxime molestias necessitatibus nobis omnis placeat porro quaerat quasi qui quidem quod, reprehenderit, similique tempore tenetur ullam unde voluptates. Incidunt maiores necessitatibus nihil placeat quas saepe soluta sunt voluptatem. Ab ad architecto atque corporis culpa cupiditate debitis dolores ea enim eos error est eum excepturi exercitationem fugiat id inventore ipsam, magni maiores molestiae neque nesciunt nisi numquam officiis perferendis porro provident quae quia rem suscipit veritatis voluptatem?</p>
+        </>
+    )
+}
+
+export default Contacts
